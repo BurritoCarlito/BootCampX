d01d2e296a5e336c0364631a65fa1c1b1701921c
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-16 21:36:02 +0000

    updated to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const { user } = require('pg/lib/defaults');
 
 const pool = new Pool({
   user: 'vagrant',
@@ -7,17 +8,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}`, limit];
+
+const queryString = 
+`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1;
+ORDER BY teacher;`
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  res.rows.forEach(user => {
+    console.log(user.cohort, ':', user.name);
   })
 });
